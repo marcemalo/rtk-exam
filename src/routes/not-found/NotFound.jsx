f34753b2a07d4756d96076f6cc04ea989422fc64
ismoiljon <mmb08092012@gmail.com>
2024-09-21 12:16:50 +0500

    rtk-exam

--- src/routes/not-found/NotFound.jsx
+++ src/routes/not-found/NotFound.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="flex items-center justify-center min-h-screen bg-blue-400 w-full mb-96">
+        <div className="max-w-[400px] flex-1 flex flex-col items-center bg-white rounded-3xl"> 
+          <h1 className=" text-2xl font-bold">Page Not Found</h1>
+        </div>
+    </div>
   )
 }
 
